c5ac4267477326f3900b6f2f09f778611b35f6d7
Alex Sindalovsky <design@acedzn.com>
2016-03-18 22:31:51 +0200

    + Added Yeoman's Angular generator

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mailLoggerApp
+ * @description
+ * # mailLoggerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mailLoggerApp', [
+    'ngAnimate'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mailLoggerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mailLoggerApp
+ */
+angular.module('mailLoggerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
